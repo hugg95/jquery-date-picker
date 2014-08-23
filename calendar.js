ccbc76f21c110c9ef6a1345d05f289ecad659c31
victor <lncwwn@gmail.com>
2014-08-23 18:15:16 +0800

    Added some functions, added demo

--- calendar.js
+++ calendar.js
@@ -1,3 +1,10 @@
+/**
+ * jquery date picker
+ * @author Victor Li
+ * @version 1.0
+ * @lincense MIT
+ */
+
 (function($) {
 
 	var defaults = {
@@ -26,14 +33,19 @@
     	cnLong: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
     };
 
+    var current = new Date(),
+    	currYear = current.getFullYear(),
+    	currMonth = current.getMonth() + 1,
+    	currDate = current.getDate();
+
     /**
      * render the calendar
      */
-    var render = function(options) {
+    var init = function(options) {
     	var container = options.container,
     		language = options.language;
         $(container).html($(_html));
-        
+
         // render weeks
         var weeksPanel = $('#calendar').find('.cal-weeks'),
         	weeksData = weeks[language + 'Short'];
@@ -42,17 +54,26 @@
         	weeksPanel.append(perDay.text(d));
         });
 
-        // render dates
+        // render some cells to show date
         var datesPanel = $('#calendar').find('.cal-body');
         for (var i = 1; i <= 5; i++) {
         	var perWeek = $('<div class="cal-per-week"></div>');
-        	for (var j = (i - 1) * 7 + 1; j <= i * 7 && j <= 31; j++) {
+        	for (var j = (i - 1) * 7 + 1; j <= i * 7; j++) {
         		var perDate = $('<span class="cal-per-date"></span>');
-        		perDate.text(j);
         		perWeek.append(perDate);
         	}
         	datesPanel.append(perWeek);
         }
+    };
+
+    var renderCells = function() {
+    	var firstDayInMonth = getFirstDayInMonth(current),
+    		datesInMonth = getDatesInMonth(current);console.log(firstDayInMonth-3);
+    	var cells = $('.cal-per-date:gt(' + (firstDayInMonth - 2) + ')');
+    		for (var i = 1; i <= datesInMonth; i++) {
+    			var dataDate = format.call(current);
+    			$(cells[i - 1]).text(i).addClass('has-date').attr('data-date', dataDate);
+    		}
     };
 
     var getFirstDayInMonth = function(date) {
@@ -68,8 +89,49 @@
     	return parseInt((dateOfNextMonth.getTime() - date.getTime()) / 1000 / 60 / 60 / 24);
     };
 
+    var format = function(format) {
+    	if (!this instanceof Date) {
+    		return;
+    	}
+
+        var o = {
+            y: this.getFullYear(),
+            m: this.getMonth() + 1,
+            d: this.getDate(),
+            hh: this.getHours(),
+            mm: this.getMinutes(),
+            ss: this.getSeconds()
+        };
+
+    	var formatted;
+
+    	switch (format) {
+    		case 'yyyy/mm/dd':
+                formatted = o.y + '/' + o.m + '/' + o.d;
+                break;
+            case 'yyyy-mm-dd':
+                formatted = o.y + '-' + o.m + '-' + o.d;
+                break;
+            case 'yyyy/mm/dd hh:mm:ss':
+                formatted = o.y + '/' + o.m + '/' + o.d + ' ' + o.hh + ':' + o.mm + ':' + o.ss;
+                break;
+            case 'yyyy-mm-dd hh:mm:ss':
+                formatted = o.y + '/' + o.m + '/' + o.d + ' ' + o.hh + ':' + o.mm + ':' + o.ss;
+                break;
+            case 'hh:mm:ss':
+                 formatted = o.hh + ':' + o.mm + ':' + o.ss;
+                 break;
+             default:
+                 formatted = o.y + '/' + o.m + '/' + o.d;
+                 break;
+    	}
+        return formatted;
+
+    };
+
     $.fn.calendar = function(options) {
         var settings = $.extend(defaults, options);
-        render(settings);
+        init(settings);
+        renderCells();
     };
  })(jQuery);
